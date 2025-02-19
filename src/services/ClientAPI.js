5df1f26ab5b5b6e0a1845c379c808513e1d8f87c
Ronit Fozdar <53905519+rony840@users.noreply.github.com>
2025-02-18 21:14:53 -0500

    fixed baseurl issues for android as it doesnt support localhost directly

--- src/services/ClientAPI.js
+++ src/services/ClientAPI.js
@@ -1,25 +1,35 @@
 import axios from 'axios';
 import KeychainHelper from '../helpers/TokenHelper';
+import { Platform } from 'react-native';
+
+// Determine the baseURL based on the platform
+const getBaseURL = () => {
+  if (Platform.OS === 'ios') {
+    // For iOS Simulator
+    return 'http://localhost:8000/api';
+  } else if (Platform.OS === 'android') {
+    // For Android Emulator
+    return 'http://10.0.2.2:8000/api';
+  }
+};
 
 // Create an Axios instance
 const ClientAPI = axios.create({
-  baseURL: 'http://10.0.2.2:8000/api', // Change this to your server URL
-  timeout: 10000,
-});// for android http://10.0.2.2:8000/api
-//for ios http://localhost:8000/api
+  baseURL: getBaseURL(), // Use dynamic baseURL based on platform
+});
 
 // Request interceptor to add Authorization token (if needed)
 ClientAPI.interceptors.request.use(
-    async (config) => {
-        const token = await KeychainHelper.getToken();
-        if (token) {
-          config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
+  async (config) => {
+    const token = await KeychainHelper.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // Response interceptor to handle errors
@@ -28,7 +38,6 @@ ClientAPI.interceptors.response.use(
     return response;
   },
   (error) => {
-    
     if (error.response) {
       // The server responded with a status code outside the range 2xx
       console.error('Response error:', error.response);
